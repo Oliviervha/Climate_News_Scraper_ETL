1f10ecbb64a42fd983d7eb6390ac6c3732b8ffdc
Olivier <39828550+Oliviervha@users.noreply.github.com>
2023-01-27 11:41:03 +0100

    Added databased load function

--- flow.py
+++ flow.py
@@ -4,6 +4,8 @@ import dask.dataframe as dd
 from newsscraper import ClimateNews
 from textblob import TextBlob
 import pandas as pd 
+import sqlite3
+from sqlite3 import Error
 
 @flow
 def run_flow():
@@ -54,8 +56,36 @@ def classify_sentiment(df:dd) -> dd:
 
 @task
 def load_news(df) -> None:
-    # TODO: load dataframe to sql lite database
-    print(df.compute())
+    
+    # Connection string
+    conn = sqlite3.connect("climatenews.db")
+
+    # Cursor object
+    cursor = conn.cursor()
+
+    # Creating table (if not exists)
+    table = """ CREATE TABLE IF NOT EXISTS CLIMATENEWS (
+                    title VARCHAR(255) NOT NULL,
+                    content VARCHAR(255) NOT NULL,
+                    date DATETIME() NOT NULL,
+                    sentiment VARCHAR(8) NOT NULL
+            ); """
+    cursor.execute(table)
+    
+    # Insert data
+    insert_query = ''' INSERT OR IGNORE INTO CLIMATENEWS(title,content,date,sentiment)
+              VALUES {}'''.format(df.compute().to_records(index=False))
+    cursor.execute(insert_query)
+    
+    # Commit the transaction
+    conn.commit()
+    
+    # Check
+    # print(pd.read_sql("SELECT * FROM CLIMATENEWS, conn))
+    
+    # Close the connection
+    conn.close()
+  
 
 if __name__ == "__main__":
     run_flow()
